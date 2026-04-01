ecdd0db2f614c3562d71789bb3c4759bd80cd668
Rahul Kumar <rahulmaurya886396@gmail.com>
2026-04-01 14:36:34 +0530

    for handeling file upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,3 +9,21 @@ function App() {
   const [resumeText, setResumeText] = useState("");
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState("");
+  const handleFileUpload = (e) => {
+    
+    const file = e.target.files[0];
+    if (!file) return; 
+
+    if (file.type === "text/plain") {
+      const reader = new FileReader();
+      
+      reader.onload = (event) => {
+        setResumeText(event.target.result); 
+      };
+      reader.readAsText(file);
+      
+    } else {
+  
+      alert("Smart Tip: Abhi ke liye apna resume .txt format mein upload karein, ya box mein direct paste karein!");
+    }
+  };
