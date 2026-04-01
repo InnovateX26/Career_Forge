8d0cd4753c82bdf91e0222e5c8b9660dbaaecacc
Rahul Kumar <rahulmaurya886396@gmail.com>
2026-04-01 13:25:15 +0530

    new post api for matching resume and job description

--- Backend/routes/resumeRoutes.js
+++ Backend/routes/resumeRoutes.js
@@ -7,3 +7,6 @@ const upload = multer({ storage: multer.memoryStorage() });
 const { generateResumeAndRoadmap, analyzeResumeMatch } = require('../controllers/resumeController');
 
 router.post('/generate', generateResumeAndRoadmap);
+
+router.post('/analyze', upload.single('resumeFile'), analyzeResumeMatch);
+module.exports = router;
