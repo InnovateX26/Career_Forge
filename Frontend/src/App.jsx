a6c2f21704da3a42370d42335477f2490fb95caa
Rahul Kumar <rahulmaurya886396@gmail.com>
2026-04-01 14:27:42 +0530

    Update App.jsx
    jd store job descreption and user resume text loading for api call and result store ai roadmap which come from backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,3 +2,10 @@ import { useState } from 'react';
 
 import axios from 'axios';
 import './App.css';
+
+function App() {
+ 
+  const [jd, setJd] = useState("");
+  const [resumeText, setResumeText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState("");
